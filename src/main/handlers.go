95d30571d62490c0d7b05e0d203b8b4bcdbe9cb4
Przemysław Saracen <p.saracen@gmail.com>
2019-10-27 21:50:17 +0100

    Add comments to handlers.go

--- src/main/handlers.go
+++ src/main/handlers.go
@@ -13,6 +13,8 @@ import (
 	"strconv"
 )
 
+// Custom middleware used to return HTTP status code 413 when the request payload exceeds 1MB
+// I don't know if it's the right way to do this but I've tried to improvise ;)
 func ConstrainPayload(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.WithFields(log.Fields{
@@ -21,6 +23,7 @@ func ConstrainPayload(next http.Handler) http.Handler {
 		}).Debug("Content length")
 
 		if r.ContentLength > 1 << (10 * 2) {
+			// TODO use a function for rendering errors
 			log.Error("Payload exceeding 1 MB posted")
 			w.WriteHeader(http.StatusRequestEntityTooLarge)
 			w.Write([]byte(""))
@@ -31,8 +34,11 @@ func ConstrainPayload(next http.Handler) http.Handler {
 	})
 }
 
+// TODO move the types to a separate file
+// Used to hide JSON fields we don't want to see in our responses
 type omit bool
 
+// Custom response object, used in the object list to omit the `last_check` field
 type ObjectResponse struct {
 	*object.Object
 	LastCheck omit `json:"last_check,omitempty"`
@@ -42,6 +48,7 @@ func (o ObjectResponse) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// Handler for the GET /api/fetcher resource
 func getObjectList(w http.ResponseWriter, r *http.Request)  {
 	var list []render.Renderer
 
@@ -58,6 +65,8 @@ func getObjectList(w http.ResponseWriter, r *http.Request)  {
 	render.RenderList(w, r, list)
 }
 
+// Middleware used to load an Object instance from the URL param into the context
+// If the specified object isn't found, here a 404 HTTP error is returned, or a 400 - when the URL parameter is invalid
 func getObjectContext(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		stringId := chi.URLParam(r, "objectId")
@@ -100,6 +109,7 @@ func getObjectContext(next http.Handler) http.Handler {
 	})
 }
 
+// Payload for the Object instance, used mostly to validate the POST data
 type ObjectRequest struct {
 	*object.Object
 }
@@ -123,6 +133,7 @@ func (or ObjectRequest) Bind(r *http.Request) error {
 	return nil
 }
 
+// Another custom response for Object, this one strips fields other than the id.
 type ObjectIdResponse struct {
 	*object.Object
 	Url omit `json:"url,omitempty"`
@@ -134,10 +145,12 @@ func (o ObjectIdResponse) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// Handler for the POST api/fetcher resource.
 func postObject(w http.ResponseWriter, r *http.Request) {
 	data := &ObjectRequest{}
 
 	if e := render.Bind(r, data); e != nil {
+		// TODO use a function for rendering errors
 		log.WithFields(log.Fields{"error": e}).Error("Error creating new Object")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(""))
@@ -147,6 +160,7 @@ func postObject(w http.ResponseWriter, r *http.Request) {
 	o := data.Object
 
 	if err := o.Save(); err != nil {
+		// TODO use a function for rendering errors
 		log.WithFields(log.Fields{"error": err.Error()}).Error("Error saving object")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(""))
@@ -159,11 +173,13 @@ func postObject(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+// Handler for the DELETE /api/fetcher/<id> resource.
 func deleteObject(w http.ResponseWriter, r *http.Request) {
 	o := r.Context().Value("object").(*object.Object)
 	log.WithFields(log.Fields{"object": o}).Info("Deleting object")
 
 	if err := o.Delete(); nil != err {
+		// TODO use a function for rendering errors
 		log.WithFields(log.Fields{"error": err.Error()}).Error("Error deleting object")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(""))
@@ -175,6 +191,7 @@ func deleteObject(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// Custom response object for the ObjectHistory instances.
 type ObjectHistoryResponse struct {
 	*object.ObjectHistory
 }
@@ -183,6 +200,7 @@ func (o ObjectHistoryResponse) Render(w http.ResponseWriter, r *http.Request) er
 	return nil
 }
 
+// Handler for the GET api/fetcher/<id>/history resource.
 func getObjectHistory(w http.ResponseWriter, r *http.Request)  {
 	o := r.Context().Value("object").(*object.Object)
 	log.WithFields(log.Fields{"object": o}).Info("Retrieving object history")
@@ -191,6 +209,7 @@ func getObjectHistory(w http.ResponseWriter, r *http.Request)  {
 	history, err := o.GetHistory()
 
 	if nil != err {
+		// TODO use a function for rendering errors
 		log.WithFields(log.Fields{"error": err.Error()}).Error("Error getting object history")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(""))
@@ -202,6 +221,7 @@ func getObjectHistory(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	if 0 == len(list) {
+		// A workaround for an empty list, because the renderer renders 'null' instead of an empty array
 		w.Write([]byte("[]"))
 		return
 	}
